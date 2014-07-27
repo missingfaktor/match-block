9b3c5e5d5bc91088453c0f9f73c7b39c83781a2c
Rahul Goma Phulore <rahul.phulore.999@gmail.com>
2014-07-28 01:45:06 +0530

    Deleting all the midje tests. I am stamping it as a failed experiment. Perhaps will spike on speclj next.

--- test/match_block/test_util.clj
+++
@@ -1,9 +0,0 @@
-(ns match-block.test-util
-  (:use [midje.sweet]))
-
-(defmacro facts-about [x & rest]
-  `(facts ~(str
-             "about "
-             (cond (symbol? x) (str "`" x "`")
-                   :else x))
-          ~@rest))
